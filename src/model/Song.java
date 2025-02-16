57c1692b0ef355ee113b04835e3d3047b4096dee
Jose Santiago Campa Morales <jscm1607@arizona.edu>
2025-02-16 01:21:20 -0700

    read files in MusicStore and initial methods in LibraryModel.

--- src/model/Song.java
+++ src/model/Song.java
@@ -1,5 +1,53 @@
 package model;
 
 public class Song {
-
+	// INSTANCE VARIABLES
+	private String title;
+	private String artist;
+	private int rating;
+	private boolean favorite;
+	
+	
+	// CONSTRUCTOR
+	public Song(String title, String artist) {
+		this.title = title;
+		this.artist = artist;
+		this.rating = 0;
+		this.favorite = false;
+	}
+	
+	// SETTERS AND GETTERS
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getArtist() {
+		return artist;
+	}
+	
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+	
+	public int getRating() {
+		return rating;
+	}
+	
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	
+	public boolean isFavorite() {
+		return favorite;
+	}
+	
+	public void setFavorite(boolean favorite) {
+		this.favorite = favorite;
+	}
+	
+	
 }
