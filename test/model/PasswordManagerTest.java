d39f2806f7ee07c1e65041b37e4ecd909fe9b77f
Cody Rice <79558669+crice114@users.noreply.github.com>
2025-03-20 21:23:12 -0700

    Implemented PBKDF2 hashing with PasswordManager and updated tests

--- test/model/PasswordManagerTest.java
+++ test/model/PasswordManagerTest.java
@@ -1,30 +1,6 @@
 package model;
 
-//
-////import model.PasswordManager;
-//import org.junit.jupiter.api.Test;
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//public class PasswordManagerTest {
-//
-//    @Test
-//    void testHashPasswordAndVerify() {
-//        String password = "secret123";
-//        String hashed = PasswordManager.hashPassword(password);
-//
-//        assertNotNull(hashed, "the hash should not be null");
-//        assertTrue(PasswordManager.verifyPassword(password, hashed), "password not verified correctly");
-//        assertFalse(PasswordManager.verifyPassword("wrongPassword", hashed), "an incorrect password should not verify");
-//    }
-//
-//    @Test
-//    void testDifferentPasswordsGiveDifferentHashes() {
-//        String hash1 = PasswordManager.hashPassword("password1");
-//        String hash2 = PasswordManager.hashPassword("password2");
-//
-//        assertNotEquals(hash1, hash2, "different hashes required for different hashes");
-//    }
-//}
+
 
 
 import  org.junit.jupiter.api.Test;
